879d2dd242426fc972aa58b23cb773b50782965c
rhardjono <14204469+rhardjono@users.noreply.github.com>
2018-01-03 18:47:46 +0100

    Add files via upload

--- Modulo01 - Data101/scripts_extra/script_parte2_opcional.sql
+++ Modulo01 - Data101/scripts_extra/script_parte2_opcional.sql
@@ -2,6 +2,7 @@
 # Separar el campo DE_DIRECCION de la tabla de ODS.ODS_HC_DIRECCIONES en dos campos: NOMBRE_VIA y NUM_VIA
 # Para realizar esta operacion hemos comprobado que el campo DE_DIRECCION tiene un formato comun consistente en:
 # numero_via(espacio)nombre_via
+# Se utiliza la función SUBSTRING_INDEX() para extraer las dos partes que componen la dirección.
 
 SELECT 
 SUBSTRING_INDEX(DE_DIRECCION, ' ', -1) NOMBRE_VIA,
@@ -12,6 +13,8 @@ ORDER BY NOMBRE_VIA;
 
 ###########################################################################################################
 
+#OPCION MENOS OPTIMA
+/*
 SELECT 
 CASE 
 	WHEN ID_DIRECCION=999999 THEN 'DESCONOCIDO'
@@ -23,3 +26,4 @@ CASE
     ELSE SUBSTRING_INDEX(DE_DIRECCION, ' ', 1) END NUM_VIA
 FROM ODS.ODS_HC_DIRECCIONES 
 ORDER BY NUM_VIA DESC;
+*/
